53abcde9a0cb1e63ef0a720d7b7502b7edfaf62a
LysenkovVA <lysenkov.v.a@yandex.ru>
2024-05-03 18:18:01 +0300

    add logout and refresh at auth.controller.ts

--- src/core/auth/auth.controller.ts
+++ src/core/auth/auth.controller.ts
@@ -2,12 +2,14 @@ import {
     BadRequestException,
     Body,
     Controller,
+    Get,
     Post,
+    Req,
     Res,
 } from "@nestjs/common"
 import { AuthService } from "./auth.service"
 import { SignUpDto } from "./dto/sign-up.dto"
-import { Response } from "express"
+import { Request, Response } from "express"
 import { ConfigService } from "@nestjs/config"
 import { SignInDto } from "./dto/sign-in.dto"
 
@@ -70,5 +72,55 @@ export class AuthController {
         }
 
         return result
+    }
+
+    @Post("/logout")
+    async logout(
+        @Req() request: Request,
+        @Res({ passthrough: true }) response: Response,
+    ) {
+        // Получаем Cookie из запроса
+        const { refreshToken } = request.cookies
+
+        if (refreshToken) {
+            // Удаляем токен из БД
+            const result = await this.authService.logout(refreshToken)
+            // Чистим Cookies из ответа
+            response.clearCookie("refreshToken")
+            response.status(200)
+            return result
+        }
+
+        response.status(200)
+    }
+
+    @Get("/refresh")
+    async refresh(
+        @Req() request: Request,
+        @Res({ passthrough: true }) response: Response,
+    ) {
+        // Получаем Cookie из запроса
+        const { refreshToken } = request.cookies
+
+        if (refreshToken) {
+            const result =
+                await this.authService.refreshAccessToken(refreshToken)
+            response.status(200)
+            // Добавляем Cookie в ответ
+            response.cookie("refreshToken", result.refreshToken, {
+                httpOnly: true,
+                // Для HTTPS
+                // secure: this.configService.get<boolean>("COOKIE_SECURE"),
+                // Столько же, сколько и рефреш токен
+                maxAge: this.configService.get<number>(
+                    "COOKIE_REFRESH_TOKEN_MAX_AGE",
+                ),
+            })
+
+            return result
+        } else {
+            response.status(500)
+            response.errored.message = "Не был найден Refresh-токен"
+        }
     }
 }
